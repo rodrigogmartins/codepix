e6812a61dbb7be148d97430de1de4e0adb005d89
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2021-02-03 19:31:34 -0300

    Adicionando conexao com o DB e operacoes dos models

---
+++ infrastructure/repository/pix.go
@@ -0,0 +1,78 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/codeedu/imersao/codepix-go/domain/model"
+	"gorm.io/gorm"
+)
+
+type PixKeyRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (pixKeyRepositoryDb *PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
+	err := pixKeyRepositoryDb.Db.Create(bank).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pixKeyRepositoryDb *PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
+	var bank model.Bank
+
+	pixKeyRepositoryDb.Db.First(&bank, "id = ?", id)
+
+	if bank.ID == "" {
+		return nil, fmt.Errorf("no account found")
+	}
+
+	return &bank, nil
+}
+
+func (pixKeyRepositoryDb *PixKeyRepositoryDb) AddAccount(account *model.Account) error {
+	err := pixKeyRepositoryDb.Db.Create(account).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pixKeyRepositoryDb *PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
+	var account model.Account
+
+	pixKeyRepositoryDb.Db.Preload("Bank").First(&account, "id = ?", id)
+
+	if account.ID == "" {
+		return nil, fmt.Errorf("no account found")
+	}
+
+	return &account, nil
+}
+
+func (pixKeyRepositoryDb *PixKeyRepositoryDb) RegisterKey(pixKey *model.PixKey) (*model.PixKey, error) {
+	err := pixKeyRepositoryDb.Db.Create(pixKey).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pixKey, nil
+}
+
+func (pixKeyRepositoryDb *PixKeyRepositoryDb) FindKeyByKind(key string, kind string) (*model.PixKey, error) {
+	var pixKey model.PixKey
+
+	pixKeyRepositoryDb.Db.Preload("Account.Bank").First(&pixKey, "kind = ? and key = ?", kind, key)
+
+	if pixKey.ID == "" {
+		return nil, fmt.Errorf("no key was found")
+	}
+
+	return &pixKey, nil
+}
---
+++ infrastructure/repository/transaction.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/codeedu/imersao/codepix-go/domain/model"
+	"gorm.io/gorm"
+)
+
+type TransactionRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (transactionRepositoryDb *TransactionRepositoryDb) Register(transaction *model.Transaction) error {
+	err := transactionRepositoryDb.Db.Create(transaction).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (transactionRepositoryDb *TransactionRepositoryDb) Save(transaction *model.Transaction) error {
+	err := transactionRepositoryDb.Db.Save(transaction).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (transactionRepositoryDb *TransactionRepositoryDb) Find(id string) (*model.Transaction, error) {
+	var transaction model.Transaction
+
+	transactionRepositoryDb.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
+
+	if transaction.ID == "" {
+		return nil, fmt.Errorf("no transaction found")
+	}
+
+	return &transaction, nil
+}
